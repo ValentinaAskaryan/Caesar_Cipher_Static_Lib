293533a76144d0cbb2020bc32450aa629f7f58f1
Valentina Askaryan <valentina.askaryan99@gmail.com>
2025-07-27 01:21:41 +0400

    Adding the header files.

---
+++ inc/decrypt.hpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+std::string decrypt(std::string, int);
---
+++ inc/encrypt.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+// Declaration of encode function.
+// Takes the plain text and the key.
+std::string encrypt(std::string, int);
